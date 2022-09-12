23eeff6182b703d4780978b32fe5b07643048401
Ugo Chiori <ugochiori@gmail.com>
2022-09-12 09:14:18 +0100

    ugo

---
+++ src/misc.js
@@ -0,0 +1,8 @@
+return (
+    <nav className="nav-links">
+      <a href="https://github.com/UgoChiori">PROJECTS</a> 
+      <a href="https://twitter.com/AdaOhafia">CONTACT</a>
+      <a href="https://adaohafia1.blogspot.com/">BLOG</a>
+
+    </nav>
+  )
