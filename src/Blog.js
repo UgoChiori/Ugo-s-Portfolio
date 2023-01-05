e1f067d695a721db52593352e5662a95462f34a7
Ugo Chiori <ugochiori@gmail.com>
2023-01-05 11:58:33 +0100

    blog component

--- src/Blog.js
+++ src/Blog.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Footer from "./Components/Footer";
 import UgoImg1 from './Components/UgoImg1';
+import "./blog.css"
+import Posts from './Components/posts/Posts';
 
 
 const Blog = () => {
   return (
-    <div>
-      <UgoImg1 heading="BLOG." text="My thoughts on programming, development and everything" />
+    <div className='blog'>
+    
+      <UgoImg1 heading="BLOG." text="CURRENTLY UNAVAILABLE. "
+  My thoughts on programming development and everything
+      
+     
+       />
+     <Posts />
+     <Posts />
+     <Posts />
+     <Posts />
       <Footer />
     </div>
   )
