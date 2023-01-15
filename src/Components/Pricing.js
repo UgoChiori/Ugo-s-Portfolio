c494282d339c084a2ce29bcb32fc4ff2fb377bb7
Ugo Chiori <ugochiori@gmail.com>
2023-01-15 09:08:29 +0100

    pricing updated

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,9 +1,9 @@
 import "./pricing.css";
 import React from "react";
-// import { Link } from "react-router-dom";
-import ugo from "../assets/ugo.mp4";
-import ugowalk from "../assets/ugowalk.mp4";
-import esspresso from "../assets/esspresso.jpeg";
+import { Link } from "react-router-dom";
+// import ugo from "../assets/ugo.mp4";
+// import ugowalk from "../assets/ugowalk.mp4";
+// import esspresso from "../assets/esspresso.jpeg";
 // import Media from "react-media";
 
 const Pricing = () => {
@@ -14,44 +14,44 @@ const Pricing = () => {
     
         <div className="card">
           
-          <video player className="intro" src={ugowalk} alt="workspace" />
-            {/* <h3>- Basic -</h3>
+          {/* <video player className="intro" src={ugowalk} alt="workspace" /> */}
+             <h3>- Basic -</h3>
             <span className="bar"></span>
-            <p className="btc">₦ 100,000</p>
-            <p>- 7 Days -</p>
+            <p className="btc">₦150,000</p>
+            <p>- 12 Days -</p>
             <p>3 pages</p>
             <p>-  Featured -</p>
-            <p>- Responsive Design -</p> */}
-            {/* <Link to="/contact" className="btn">
+            <p>- Responsive Design -</p> 
+             <Link to="/contact" className="btn">
                 BUY NOW
-            </Link> */}
+            </Link> 
         </div>
         <div className="card">
-          <video player className="intro" src={ugo} alt="workspace" />
+          {/* <video player className="intro" src={ugo} alt="workspace" /> */}
         
-            {/* <h3>- Advanced -</h3>
+            <h3>- Advanced -</h3>
             <span className="bar"></span>
-            <p className="btc">₦ 250,000</p>
-            <p>-  5 Days -</p>
+            <p className="btc">₦300,000</p>
+            <p>-  7 Days -</p>
             <p>4 pages</p>
             <p>-  Featured -</p>
-            <p>- Responsive Design -</p> */}
-            {/* <Link to="/contact" className="btn">
+            <p>- Responsive Design -</p>
+             <Link to="/contact" className="btn">
                 BUY NOW
-                </Link> */}
+                </Link>
                 </div>
                 <div className="card">
-                <img className="intro" src={esspresso} alt="workspace" />
-                     {/* <h3>- Premium -</h3> */}
-                    {/* <span className="bar"></span>
-                     <p className="btc">₦ 400,000</p>
-                     <p>- 3 Days -</p>
+                {/* <img className="intro" src={esspresso} alt="workspace" /> */}
+                      <h3>- Premium -</h3> 
+                     <span className="bar"></span>
+                     <p className="btc">₦500,000</p>
+                     <p>- 5 Days -</p>
                      <p>6 pages</p>
                      <p>-  Featured -</p>
-                     <p>- Responsive Design -</p>  */}
-                    {/* <Link to="/contact" className="btn">
+                     <p>- Responsive Design -</p>  
+                     <Link to="/contact" className="btn">
                         BUY NOW
-                    </Link> */}
+                    </Link> 
                     </div>
 
       </div>
