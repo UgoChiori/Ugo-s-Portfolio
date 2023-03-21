7821bf25904d49188787341a28afb6a3e0531f35
Ugo Chiori <ugochiori@gmail.com>
2023-03-21 22:25:42 +0100

    stack icons

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,7 +1,18 @@
 import "./landingpage.css";
 import ugodawn from "../assets/ugodawn.jpeg";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import React from "react";
+
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaReact,
+  FaCss3,
+  FaDev,
+  FaVuejs
+} from "react-icons/fa";
+
 // import { useTranslation } from "react-i18next";
 // import { changeLanguage } from "i18next";
 // import i18next from "i18next";
@@ -24,7 +35,7 @@ function LandingPage() {
       </div>
       <div className="content">
         <h1>Front End Developer</h1>
-        <h4>Baker turned developer, </h4>
+        <h4>Baker Turned Developer, </h4>
         <p> I enjoy writing code, solving problems and building things. </p>
         <div>
           <Link to="/project" className="btn">
@@ -33,6 +44,65 @@ function LandingPage() {
           <Link to="/contact" className="btn-light">
             Contact
           </Link>
+        </div>
+        <div>
+          <FaDev
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
+          <FaHtml5
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
+          <FaCss3
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
+          <FaJsSquare
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
+          <FaReact
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
+
+          <FaCss3Alt
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
+          <FaVuejs
+            size={20}
+            style={{
+              color: "white",
+              cursor: "pointer",
+              marginRight: "2rem",
+            }}
+          />
         </div>
       </div>
     </div>
