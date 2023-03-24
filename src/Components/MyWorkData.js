66ba44615eb6c7f31717ba8b7042bcce648fa8c9
Ugo Chiori <ugochiori@gmail.com>
2023-03-24 16:41:10 +0100

    new project added

--- src/Components/MyWorkData.js
+++ src/Components/MyWorkData.js
@@ -1,5 +1,5 @@
 // import workspace1 from "../assets/workspace1.png";
-import girlcupcake from "../assets/girlcupcake.jpeg";
+import chef from "../assets/chef.png";
 import Todo from "../assets/Todo.jpeg";
 import coffeeshop from "../assets/coffeeshop.webp";
 import shopcandles1 from "../assets/shopcandles1.jpeg";
@@ -17,11 +17,11 @@ const ProjectCardData = [
     code: "https://github.com/UgoChiori/ReactRouter-Assignment",
   },
   {
-    imgsrc: girlcupcake,
-    title: "Hire -a- Baker",
-    text: "A website that allows users to locate their favourite bakers online, built with React.",
-    view: "https://hire-abaker-wkkc-g6wd2lksq-ugochiori.vercel.app/",
-    code: "https://github.com/UgoChiori/HireAbaker",
+    imgsrc: chef,
+    title: "Hire -a- Chef",
+    text: "A website that allows users to locate their favourite chefs online, built with Vue Js.",
+    view: "https://hireachef.vercel.app/",
+    code: "https://github.com/UgoChiori/hireachef",
   },
   {
     imgsrc: Todo,
